adac843c315f18f7f2abb44e17dce7512967c1c0
Arnaud Hervy <22794594+ahervy@users.noreply.github.com>
2024-05-13 10:52:19 +0200

    Add and configure Astro Sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: netlify()
+  output: 'server',
+  adapter: netlify(),
+  site: 'https://arnaudhervy.com',
+  integrations: [sitemap()],
 });
