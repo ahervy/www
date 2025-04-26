dc565d56b0abfdf50ff9df3365030060aac0c8ea
Arnaud Hervy <22794594+ahervy@users.noreply.github.com>
2025-04-26 14:21:15 +0200

    Remove Netlify config (#186)
    * remove netlify config
    
    * update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import netlify from '@astrojs/netlify';
 
 const site = 'https://arnaudhervy.com';
-const excludedPaths = ['/samples/how-to/', '/samples/user-guide/'];
+const excludedPages = ['/samples/how-to/', '/samples/user-guide/'];
 
 export default defineConfig({
-  output: 'static',
-  adapter: netlify(),
   site,
+  output: 'static',
   integrations: [
     sitemap({
-      filter: (page) => !excludedPaths.includes(new URL(page).pathname),
+      filter: (page) => !excludedPages.includes(new URL(page).pathname),
     }),
   ],
 });
