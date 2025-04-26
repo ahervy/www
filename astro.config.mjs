422772687214b371bf18c76ba490ece26f43a243
Arnaud Hervy <22794594+ahervy@users.noreply.github.com>
2025-04-26 14:16:34 +0200

    remove netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import netlify from '@astrojs/netlify';
 
 const site = 'https://arnaudhervy.com';
 const excludedPaths = ['/samples/how-to/', '/samples/user-guide/'];
 
 export default defineConfig({
   output: 'static',
-  adapter: netlify(),
   site,
   integrations: [
     sitemap({
